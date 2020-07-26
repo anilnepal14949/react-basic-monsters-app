bc21178ac01633a3aeff6eb461f2a19499df4f3c
Anil Nepal <anilnepal14949@gmail.com>
2020-07-26 17:13:16 +1000

    Updates

---
+++ precache-manifest.5662af44fc4c2524b9a61086418f2c9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a46826b3e3abbc15df2dc2132ba53e",
+    "url": "/react-basic-monsters-app/index.html"
+  },
+  {
+    "revision": "4df2de03fd94e0dfe9ef",
+    "url": "/react-basic-monsters-app/static/css/main.62408a4d.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react-basic-monsters-app/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basic-monsters-app/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4df2de03fd94e0dfe9ef",
+    "url": "/react-basic-monsters-app/static/js/main.b62104fa.chunk.js"
+  },
+  {
+    "revision": "a281fa36e967a4bbbe4c",
+    "url": "/react-basic-monsters-app/static/js/runtime-main.9b8dd1f1.js"
+  }
+]);
